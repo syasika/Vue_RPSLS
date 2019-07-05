cdd52330934586e0650dcf7daa43f68c5ef38dca
Shane <syasika@cn2c.com>
2019-07-05 10:59:40 -0400

    Added logic to vuex store

--- src/store.js
+++ src/store.js
@@ -4,7 +4,36 @@ import Vuex from "vuex";
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {}
+  state: {
+    computerScore: 0,
+    playerScore: 0,
+    drawTotal: 0,
+    gameTotal: 0
+  },
+  mutations: {
+    CHANGE_SCORES(state, computerWin, isDraw) {
+      if (isDraw) {
+        state.drawTotal;
+      } else {
+        if (computerWin) {
+          state.computerScore++;
+        } else {
+          state.playerScore++;
+        }
+      }
+
+      state.gameTotal++;
+    }
+  },
+  getters: {
+    computerScore: state => state.computerScore,
+    playerScore: state => state.playerScore,
+    drawTotal: state => state.drawTotal,
+    gameTotal: state => state.gameTotal
+  },
+  actions: {
+    UPDATE_SCORE: async (context, computerWin, isDraw) => {
+      context.commit("CHANGE_SCORES", computerWin, isDraw);
+    }
+  }
 });
